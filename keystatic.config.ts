8ca0cc83bf64ccf1026b5ff51e488cba41146691
Guido X Jansen <x@gui.do>
2024-06-08 23:37:11 +0200

    Keystatic test

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,28 +1,26 @@
 import { config, fields, collection } from '@keystatic/core';
 
 export default config({
-    storage: {
-        kind: 'local',
-    },
+  storage: {
+    kind: 'local',
+  },
 
-    collections: {
-        posts: collection({
-            label: 'Posts',
-            slugField: 'title',
-            path: 'src/content/blog/*',
-            format: { contentField: 'content' },
-            schema: {
-                title: fields.string({ name: { label: 'Title' } }),
-                content: fields.string({
-                    label: 'Content',
-                }),
-                author: fields.string({
-                    label: 'Author',
-                }),
-                date: fields.date({
-                    label: 'Date',
-                }),
-            },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/blog/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.document({
+          label: 'Content',
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: true,
         }),
-    },
+      },
+    }),
+  },
 });
