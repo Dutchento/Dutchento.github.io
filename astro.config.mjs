f696506a127de331dbc9ef7845d8f9a530ceb8ef
Guido X Jansen <x@gui.do>
2024-06-08 14:15:58 +0200

    domain update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dutchento.github.io",
+  site: "https://www.dutchento.org",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
