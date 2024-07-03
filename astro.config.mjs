b9e0026dfd421a17db1d347193411732bd217461
Guido X Jansen <x@gui.do>
2024-07-03 17:02:05 +0200

    Added npx @playform/compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.dutchento.org",
-  integrations: [tailwind(), mdx(), sitemap(), icon(),],
+  integrations: [tailwind(), mdx(), sitemap(), icon(), playformCompress()]
 });
