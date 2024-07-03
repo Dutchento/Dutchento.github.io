c6e946430b49f4551d7fe717291ec5c967efee60
Guido X Jansen <x@gui.do>
2024-07-03 23:42:44 +0200

    Added Astro Compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-
 import playformCompress from "@playform/compress";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.dutchento.org",
-  integrations: [tailwind(), mdx(), sitemap(), icon(), playformCompress()]
+  integrations: [tailwind(), mdx(), sitemap(), icon(), playformCompress(), compressor()]
 });
